5bd8427c1384bf0bd3d3cceefe674af71ff02644
shahriar eshraghi <shahriareshraghi22@gmail.com>
2021-02-27 22:50:50 +0330

    Start working with Component and States

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
+import Person from './Person/Person';
+export default class App extends Component {
+  state = {
+    person:[
+      {name :'Shahriar',age:25},
+      {name :'Alex',age:23},
+      {name :'Sarah',age:22} 
+    ]
+  }
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Person name={this.props.person[0].name} age={this.props.person[0].age}/>
+       <Person name={this.props.person[1].name} age={this.props.person[1].age}>and my hobbies is : Coding</Person>
+       <Person name={this.props.person[2].name} age={this.props.person[2].age}/>
       </header>
     </div>
-  );
+    );
+  }
 }
-
-export default App;
